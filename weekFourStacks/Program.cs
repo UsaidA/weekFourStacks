2c50175bfee6d3bb174aa8292690cd14c7495a3f
Usaid Ahmed <85448432+UsaidA@users.noreply.github.com>
2021-10-20 14:03:42 +0100

    more stuff
    even more stuff

--- weekFourStacks/Program.cs
+++ weekFourStacks/Program.cs
@@ -4,9 +4,76 @@ namespace weekFourStacks
 {
     class Program
     {
+       // public static bool isFULL(in int top, in int max);
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void POSTFIX(int E)
+        {
+            int top, x;
+            
+
+        }
+
+        public static void create()
+        {
+            int top = 0;
+        }
+        public static void push(int item, ref int top, ref int stack, int max)
+        {
+            if (isFUll(top, max) = true){
+                stackFULL();
+            }
+            else
+                top = top + 1;
+                stack(top) = item;
+
+            
+        }
+        public static void pop(out int item, ref int stack[], ref int top)
+        {
+            if (isEmpty(top) = true) {
+                stackEmpty();
+            }
+            else{
+                item = stack(top);
+                top = top + 1;
+            }
+        }
+        public static int getTop(in int top, in int stack[])
+        {
+            if isEmpty(top) = true{
+                stackEmpty();
+            }
+            else
+            {
+                return stack(top);
+            }
+        }
+        public static bool isEmpty (in int top, in int stack[])
+        {
+            if (top = 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public static bool isFULL(in int top, in int max)
+        {
+            if (top = max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
 }
